bd5c2f95cea049a73237eb211fdefb8107144c59
isamallow <isabela.ribeiro1211@gmail.com>
2022-04-08 12:25:09 -0300

    Adição de métodos no Service e Atualizar Produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { MatTableModule } from "@angular/material/table";
 
 import localePt from "@angular/common/locales/pt";
 import { registerLocaleData } from "@angular/common";
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localePt);
 
@@ -42,6 +43,7 @@ registerLocaleData(localePt);
     RedDirective,
     ProductCreateComponent,
     ProductReadComponent,
+    ProductUpdateComponent,
   ],
   imports: [
     BrowserModule,
